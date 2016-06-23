6022d1a517f1948ddc2e39380cc9e68a5a6055ff
Luu,Sonny S <s.luu@student.fontys.nl>
2016-06-23 11:03:42 +0200

    Update

--- Dodge/Dodge.js
+++ Dodge/Dodge.js
@@ -1,18 +1,8 @@
 window.onload = function()
 {
     
-/* flag=1 function f1() 
-{ alert("Yes. you are right ") } 
 
-function f() 
-{ if(flag==1) 
-{ Bn.style.top=90 Bn.style.left=500 flag=2 } 
- else if(flag==2) 
- { Bn.style.top=90 Bn.style.left=50 flag=3 } 
- else if(flag==3) 
- { Bn.style.top=235 Bn.style.left=360 flag=1 } } 
- */
-console.log(Math.floor(Math.random() * 1200) + 1);
+//console.log(Math.floor(Math.random() * 1200) + 1);
 
     
 var dodge = document.getElementsByClassName("Dodge");
@@ -32,37 +22,41 @@ for (var i = 0; i < dodge.length; i++)
                                  
                                  )
     }
-
-
-
-
-
-/*
-
-
-var asociaal = document.getElementsByClassName("asociaal");
-for (var i = 0; i < asociaal.length; i++) {
-    asociaal[i].addEventListener('mouseup', function(antisocial) {
-        var x = antisocial.clientX;
-        var y = antisocial.clientY;
-        var berichten = ["Waarom klik je hier?", "Je kan niet praten met een stuk natuur", "Dit is geen persoon", "Hier is niks te vinden", "Geen interactie mogelijk hier"];
-        var bericht = berichten[Math.floor(Math.random()*berichten.length)];
-        
-        var beweeghet = document.createElement("div");
-        beweeghet.innerHTML = bericht;
-        beweeghet.setAttribute("id", "wolk");
-        beweeghet.style.left = x + "px";
-        beweeghet.style.top = y + "px";
-        beweeghet.addEventListener('animationend', function(antisocial) {
-            this.parentNode.removeChild(this);
-        });
-        document.body.appendChild(beweeghet);
-    });
-}
-
-*/    
     
-    
+var dodge2 = document.getElementsByClassName("DodgeVert");
+ for (var i = 0; i < dodge2.length; i++)
+    {
+        dodge2[i].addEventListener('mouseover', function(dodging)
+            {    
+                var number1 = Math.floor(Math.random() * 400) + 1;  
+                //var number2 = Math.floor(Math.random() * 1200) + 100;
+                //var number3 = Math.floor(Math.random() * 1200) + 100;
+                
+                this.style.paddingTop=number1;
+                //this.style.paddingLeft=number2;
+                //this.style.paddingRight=number3;
+                //console.log(dodging);
+            }                         
+                                 
+                                 )
+    }   
+var dodge3 = document.getElementsByClassName("DodgeHor");
+ for (var i = 0; i < dodge3.length; i++)
+    {
+        dodge3[i].addEventListener('mouseover', function(dodging)
+            {    
+                //var number1 = Math.floor(Math.random() * 400) + 1;  
+                var number2 = Math.floor(Math.random() * 1200) + 100;
+                var number3 = Math.floor(Math.random() * 1200) + 100;
+                
+                //this.style.paddingTop=number1;
+                this.style.paddingLeft=number2;
+                this.style.paddingRight=number3;
+                //console.log(dodging);
+            }                         
+                                 
+                                 )
+    }   
     
 }
 
